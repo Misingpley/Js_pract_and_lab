d37f264e7486c14a15bc203a1360bebc14b2b4fb
Misingpley <misingpley905@gmail.com>
2026-04-18 14:23:04 +0300

    add pract 5 pokemon card UI

--- Pract_5/index.js
+++ Pract_5/index.js
@@ -1,30 +1,52 @@
 "use strict";
 
 const btn = document.getElementById("loadPokemon");
-const output = document.getElementById("output");
+const card = document.getElementById("card");
 
 btn.addEventListener("click", async () => {
-  const name = prompt("Введи ім'я або ID покемона:");
-
-  if (!name) return;
+  const id = Math.floor(Math.random() * 151) + 1;
 
   try {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-
-    if (!res.ok) throw new Error("Pokemon not found");
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    if (!res.ok) throw new Error("Error");
 
     const data = await res.json();
 
-    const result = {
-      name: data.name,
-      height: data.height,
-      weight: data.weight,
-      abilities: data.abilities.map(a => a.ability.name)
-    };
+    renderPokemon(data);
 
-    output.textContent = JSON.stringify(result, null, 2);
-
-  } catch (err) {
-    output.textContent = "Помилка: " + err.message;
+  } catch (e) {
+    card.innerHTML = "Помилка";
   }
 });
+
+function renderPokemon(p) {
+  const types = p.types.map(t => `<span>${t.type.name}</span>`).join("");
+
+  card.innerHTML = `
+    <div class="card">
+      <div class="top">
+        <div class="hp">HP ${p.stats[0].base_stat}</div>
+        <img src="${p.sprites.front_default}" />
+      </div>
+
+      <h2>${p.name}</h2>
+
+      <div class="types">${types}</div>
+
+      <div class="stats">
+        <div>
+          <strong>${p.stats[1].base_stat}</strong>
+          <p>Attack</p>
+        </div>
+        <div>
+          <strong>${p.stats[2].base_stat}</strong>
+          <p>Defense</p>
+        </div>
+        <div>
+          <strong>${p.stats[5].base_stat}</strong>
+          <p>Speed</p>
+        </div>
+      </div>
+    </div>
+  `;
+}
